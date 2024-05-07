e9f7c2c13945222f64dac268bc4252b81779908f
dianzulyani <167389046+dianzulyani@users.noreply.github.com>
2024-05-07 21:18:58 +0700

    Add files via upload

---
+++ login.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+char Login ( ) {
+    char pass [ ] = {"password"}; //untuk password nya , ketika login kata "password" ini yg menentukan berhasil atau gagal
+    char akun;
+    char username [256];
+    char password[256];
+    printf ("login sebagai admin atau user [a/u] ?\n");
+    scanf ("%c",&akun);
+    
+    printf("USERNAME: ");
+    scanf (" %s", username);
+    printf("\nPASSWORD: ");
+    scanf (" %s", password);
+    
+
+    if (strcmp(password , pass ) == 0) {
+        printf("Login success!!\n");
+        
+    }
+    else {
+        printf("ERROR!PASSWORD ATAU USERNAME SALAH!\n");
+        EXIT_FAILURE;
+    }
+
+}
+
+int main (){
+    Login ();
+}
