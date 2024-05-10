71ac0877e5885c39999bf1fd738749d21115a8c2
DWI HAMDAN SUKRAN <166540674+dwihamdansukran@users.noreply.github.com>
2024-05-10 22:11:35 +0700

    Update header.h

--- header.h
+++ header.h
@@ -18,3 +18,32 @@ void hapus_buku(Buku daftar_buku[], int *jumlah_buku);
 void edit_buku(Buku daftar_buku[], int jumlah_buku);
 void simpan_ke_file(Buku daftar_buku[], int jumlah_buku);
 void baca_dari_file(Buku daftar_buku[], int *jumlah_buku);
+
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+typedef struct {
+    unsigned int book_id;
+    char title[100];
+    char author[50];
+    char publisher[50];
+    unsigned int pages;
+    unsigned int year;
+    unsigned int available_qty;
+} Book;
+
+typedef struct {
+    char username[20];
+    char password[20];
+    int is_admin;
+} User;
+
+void load_books_from_file();
+void save_books_to_file();
+int login(char *username, char *password);
+void print_books();
+void borrow_book(int user_index);
+void return_book();
+
+#endif
+
