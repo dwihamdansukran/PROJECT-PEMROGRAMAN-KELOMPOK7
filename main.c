e58a1e16f1386b8dd0e61050e2f00819d14af32a
DWI HAMDAN SUKRAN <166540674+dwihamdansukran@users.noreply.github.com>
2024-05-10 21:57:49 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,2 +1,49 @@
 #include "header.h"
+#include "library.h"
 
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <username> <password>\n", argv[0]);
+        return 1;
+    }
+
+    load_books_from_file(); 
+
+    int user_index = login(argv[1], argv[2]); 
+    if (user_index == -1) {
+        printf("Login gagal. Periksa kembali username dan password Anda.\n");
+        return 1;
+    }
+
+    printf("Login berhasil.\n");
+
+    int choice;
+    do {
+        printf("\nMenu:\n");
+        printf("1. Lihat Buku Tersedia\n");
+        printf("2. Pinjam Buku\n");
+        printf("3. Kembalikan Buku\n");
+        printf("4. Keluar\n");
+        printf("Pilihan Anda: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                print_books();
+                break;
+            case 2:
+                borrow_book(user_index);
+                break;
+            case 3:
+                return_book();
+                break;
+            case 4:
+                printf("Terima kasih telah menggunakan layanan kami.\n");
+                break;
+            default:
+                printf("Pilihan tidak valid.\n");
+        }
+    } while (choice != 4);
+
+    return 0;
+}
