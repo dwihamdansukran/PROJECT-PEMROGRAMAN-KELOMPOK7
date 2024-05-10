8fb3f68cc29c781eb5b24f5dc674bf21d497bcc6
DWI HAMDAN SUKRAN <166540674+dwihamdansukran@users.noreply.github.com>
2024-05-10 22:06:49 +0700

    Update admin.c

--- admin.c
+++ admin.c
@@ -1,21 +1,5 @@
 #include "header.h"
 
-typedef struct {
-    unsigned int id;
-    char judul[100];
-    char penulis[50];
-    char penerbit[50];
-    unsigned int jumlah_halaman;
-    unsigned int tahun_terbit;
-    unsigned int jumlah_tersedia;
-} Buku;
-
-void tambahkan_buku(Buku daftar_buku[], int *jumlah_buku);
-void hapus_buku(Buku daftar_buku[], int *jumlah_buku);
-void edit_buku(Buku daftar_buku[], int jumlah_buku);
-void simpan_ke_file(Buku daftar_buku[], int jumlah_buku);
-void baca_dari_file(Buku daftar_buku[], int *jumlah_buku);
-
 int main() {
     Buku daftar_buku[100];
     int jumlah_buku = 0;
