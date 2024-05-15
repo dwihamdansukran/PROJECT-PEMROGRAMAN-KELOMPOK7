4f07c0eef283d667785d594c158a62d27e03a4d3
mhRekyy <166730737+mhRekyy@users.noreply.github.com>
2024-05-16 06:21:24 +0700

    Update simpanData.c

--- simpanData.c
+++ simpanData.c
@@ -1,1 +1,25 @@
+#include "header.h"
 
+// Fungsi untuk menyimpan data buku ke file
+void simpanDataBuku()
+{
+    FILE *file = fopen(FILENAME, "w");
+    if (file == NULL)
+    {
+        printf("\nGagal membuka file.\n");
+        return;
+    }
+
+    for (int i = 0; i < jumlah_buku; ++i)
+    {
+        fprintf(file, "Id Buku: %u\n", buku[i].id);
+        fprintf(file, "Judul: %s\n", buku[i].judul);
+        fprintf(file, "Penulis: %s\n", buku[i].penulis);
+        fprintf(file, "Penerbit: %s\n", buku[i].penerbit);
+        fprintf(file, "Jumlah Halaman: %u\n", buku[i].jumlah_halaman);
+        fprintf(file, "Tahun Terbit: %u\n", buku[i].tahun_terbit);
+        fprintf(file, "Jumlah Tersedia: %u\n", buku[i].jumlah_tersedia);
+    }
+
+    fclose(file);
+}
