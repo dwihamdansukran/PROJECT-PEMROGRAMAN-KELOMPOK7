3bd4071608c2be5b6a79c4f6b7b313de60a67c4d
nadiaamgdln <166017227+nadiaamgdln@users.noreply.github.com>
2024-05-16 05:30:17 +0700

    Update header.h

--- header.h
+++ header.h
@@ -2,38 +2,55 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#ifndef LIBRARY_H
+#define LIBRARY_H
 
+#define FILENAME "books.txt"
+#define MAX_BUKU 100
+#define MAX_BUKU_DIPINJAM 10
+
+// Struktur untuk menyimpan informasi buku
 typedef struct {
     unsigned int id;
     char judul[100];
-    char penulis[50];
-    char penerbit[50];
+    char penulis[100];
+    char penerbit[100];
     unsigned int jumlah_halaman;
     unsigned int tahun_terbit;
     unsigned int jumlah_tersedia;
 } Buku;
 
-void tambahkan_buku(Buku daftar_buku[], int *jumlah_buku);
-void hapus_buku(Buku daftar_buku[], int *jumlah_buku);
-void edit_buku(Buku daftar_buku[], int jumlah_buku);
-void simpan_ke_file(Buku daftar_buku[], int jumlah_buku);
-void baca_dari_file(Buku daftar_buku[], int *jumlah_buku);
+extern Buku buku[MAX_BUKU];
+extern int jumlah_buku;
+extern int jumlah_user;
+extern int jumlah_buku_dipinjam;
+extern Buku buku_dipinjam[MAX_BUKU_DIPINJAM];
 
-#ifndef LIBRARY_H
-#define LIBRARY_H
+// Struktur untuk menyimpan informasi user
+struct User {
+    char username[100];
+    char password[100];
+    int isAdmin;
+};
 
-typedef struct {
-    char username[20];
-    char password[20];
-    int is_admin;
-} User;
+// Fungsi admin
+void tambahBuku();
+void hapusBuku();
+void editBuku();
 
-void load_books_from_file();
-void save_books_to_file();
-int login(char *username, char *password);
-void print_books();
-void borrow_book(int user_index);
-void return_book();
+// Fungsi user
+void lihatBuku();
+void pinjamBuku();
+void lihatBukuDipinjam();
+void kembalikanBuku();
+
+// Fungsi tambahan
+void login();
+void adminMenu();
+void userMenu();
+void simpanDataBuku();
+void bacaDataBuku();
+void clearBuffer();
+
 
 #endif
-
